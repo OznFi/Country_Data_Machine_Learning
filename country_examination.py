dd1aa4a4d1c6b96673e1099acb214d6eea180e2b
OzanFin <OzanFin@bitbucket.org>
2023-08-14 07:38:27 +0300

    Commented out the iterative imputer, attempting picking the features by name as index

--- country_examination.py
+++ country_examination.py
@@ -55,22 +55,23 @@ for feature in countryData.columns:
 #print(nullCounts[nullCounts > countryData.shape[0]*0.1])
 lacking_features=countryData[['Armed Forces size','Gasoline Price','Minimum wage','Tax revenue (%)']]
 non_missing_columns = [col for col in countryData.columns if col not in lacking_features.columns]
-#print(non_missing_columns)
+print(countryData.columns)
 
 #
 #IMPUTATION STEPS START HERE FOR THE LACKING FEATURES
 #
 lackingFeatureValues = lacking_features.values
 nonLackingFeatureValues = countryData.dropna(subset=lacking_features.columns)[countryData.columns].values
+nonLackingFeatureValues = countryData.dropna(subset=lacking_features.columns)[['Density','Agricultural Land( %)','Land Area(Km2)','Armed Forces size','Birth Rate','Co2-Emissions','CPI','CPI Change (%)','Fertility Rate','Forested Area (%)','Gasoline Price','GDP','Gross primary education enrollment (%)','Gross tertiary education enrollment (%)','Infant mortality','Life expectancy','Maternal mortality ratio','Minimum wage','Out of pocket health expenditure','Physicians per thousand','Population','Population: Labor force participation (%)','Tax revenue (%)','Total tax rate','Unemployment rate','Urban_population','Latitude','Longitude']]
 knnImputer=KNNImputer(n_neighbors = 5)
 knn_imputed_features=knnImputer.fit_transform(lackingFeatureValues)
 
-imputer = IterativeImputer(max_iter=10, random_state=0)
-imputer.fit(nonLackingFeatureValues)
+#imputer = IterativeImputer(max_iter=10, random_state=0)
+#imputer.fit(nonLackingFeatureValues)
 
-imputedFeatures = imputer.transform(knn_imputed_features)
-imputedFeatures = pd.DataFrame(imputedFeatures, columns=lacking_features.columns)
-print(imputedFeatures)
+#imputedFeatures = imputer.transform(knn_imputed_features)
+#imputedFeatures = pd.DataFrame(imputedFeatures, columns=lacking_features.columns)
+print(knn_imputed_features)
 
 
 #print(lacking_features.describe(include='all'))
