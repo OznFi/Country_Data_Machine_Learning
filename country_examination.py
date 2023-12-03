8f25015e8b06eff22b0d61b742fefe2dfb8bed01
OzanFin <OzanFin@bitbucket.org>
2023-12-03 23:19:26 +0300

    AdjustFeatures function fix

--- country_examination.py
+++ country_examination.py
@@ -5,7 +5,6 @@ Created on Wed Jul 19 20:59:07 2023
 @author: ZBOOK 15 G5
 """
 import warnings
-import numpy as np
 import pandas as pd
 import sklearn.mixture as gaussianMixture
 from sklearn.experimental import enable_iterative_imputer
@@ -23,12 +22,13 @@ def adjustFeatures(value):
             value = value.replace('$','')
             value = value.replace('%','')
             value = value.replace(',','')
-            value = float(value)
+            return(float(value))
         else:
             value = value.replace('$','')
             value = value.replace('%','')
             value = value.replace(',','')
-            value = float(value)            
+            return(float(value))
+    return value            
             
         
         
@@ -37,6 +37,8 @@ countryIndexedData = countryData.set_index('Country')
 print(countryData.shape)
 #there are 195 entries, 35 features
 print(countryData.dtypes)
+
+
 
 #Features are cleaned of percentages, dollar signs and commas
 nonNumericFeatures=['Country','Abbreviation','Capital/Major City','Currency-Code','Largest city','Official language']
@@ -55,23 +57,25 @@ for feature in countryData.columns:
 #print(nullCounts[nullCounts > countryData.shape[0]*0.1])
 lacking_features=countryData[['Armed Forces size','Gasoline Price','Minimum wage','Tax revenue (%)']]
 non_missing_columns = [col for col in countryData.columns if col not in lacking_features.columns]
-print(countryData.columns)
+#print(countryData.columns)
 
 #
 #IMPUTATION STEPS START HERE FOR THE LACKING FEATURES
 #
 lackingFeatureValues = lacking_features.values
 nonLackingFeatureValues = countryData.dropna(subset=lacking_features.columns)[countryData.columns].values
-nonLackingFeatureValues = countryData.dropna(subset=lacking_features.columns)[['Density','Agricultural Land( %)','Land Area(Km2)','Armed Forces size','Birth Rate','Co2-Emissions','CPI','CPI Change (%)','Fertility Rate','Forested Area (%)','Gasoline Price','GDP','Gross primary education enrollment (%)','Gross tertiary education enrollment (%)','Infant mortality','Life expectancy','Maternal mortality ratio','Minimum wage','Out of pocket health expenditure','Physicians per thousand','Population','Population: Labor force participation (%)','Tax revenue (%)','Total tax rate','Unemployment rate','Urban_population','Latitude','Longitude']]
+#nonLackingFeatureValues = countryData.dropna(subset=lacking_features.columns)[['Density','Agricultural Land( %)','Land Area(Km2)','Armed Forces size','Birth Rate','Co2-Emissions','CPI','CPI Change (%)','Fertility Rate','Forested Area (%)','Gasoline Price','GDP','Gross primary education enrollment (%)','Gross tertiary education enrollment (%)','Infant mortality','Life expectancy','Maternal mortality ratio','Minimum wage','Out of pocket health expenditure','Physicians per thousand','Population','Population: Labor force participation (%)','Tax revenue (%)','Total tax rate','Unemployment rate','Urban_population','Latitude','Longitude']]
 knnImputer=KNNImputer(n_neighbors = 5)
 knn_imputed_features=knnImputer.fit_transform(lackingFeatureValues)
+countryData.loc[:, lacking_features.columns] = knn_imputed_features
 
+print(countryData)
 #imputer = IterativeImputer(max_iter=10, random_state=0)
 #imputer.fit(nonLackingFeatureValues)
 
 #imputedFeatures = imputer.transform(knn_imputed_features)
 #imputedFeatures = pd.DataFrame(imputedFeatures, columns=lacking_features.columns)
-print(knn_imputed_features)
+#print(knn_imputed_features)
 
 
 #print(lacking_features.describe(include='all'))
